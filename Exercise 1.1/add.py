c18d8b0d518dcf4ef25d425198cc93800e1c8b17
freak906 <119906147+nick-vns@users.noreply.github.com>
2023-06-27 15:49:52 -0600

    Initial commit

---
+++ Exercise 1.1/add.py
@@ -0,0 +1,11 @@
+# user's input 1
+a = int(input("Enter a number: "))
+
+# user's input 22
+b = int(input("Enter a number: "))
+
+# combine value
+c = a + b
+
+# recieve output
+print("Your summary is: ", c)
