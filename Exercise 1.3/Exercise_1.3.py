81ed83cd4b39c52d76ece3efcf6bd4dec964998c
freak906 <119906147+nick-vns@users.noreply.github.com>
2023-07-07 15:24:44 -0600

    installed black

--- Exercise 1.3/Exercise_1.3.py
+++ Exercise 1.3/Exercise_1.3.py
@@ -7,9 +7,9 @@ def take_recipe():
     cooking_time = int(input("Enter cooking time in minuets: "))
     ingredients = input("Enter ingredients exp(Egg, Soy, etc): ").title()
     recipe = {
-        'name': name,
-        'cooking_time': cooking_time,
-        'ingredients': ingredients.split(', ')
+        "name": name,
+        "cooking_time": cooking_time,
+        "ingredients": ingredients.split(", "),
     }
     return recipe
 
@@ -19,7 +19,7 @@ n = int(input("How many recipes would you like to enter? "))
 for i in range(n):
     recipe = take_recipe()
 
-    for ingredient in recipe['ingredients']:
+    for ingredient in recipe["ingredients"]:
         if ingredient not in ingredients_list:
             ingredients_list.append(ingredient)
         # elif ingredient in ingredients_list:
@@ -27,18 +27,32 @@ for i in range(n):
     recipes_list.append(recipe)
 
 for recipe in recipes_list:
-    if recipe['cooking_time'] < 10 and len(recipe['ingredients']) < 4:
-        recipe['difficulty'] = "Easy"
-    elif recipe['cooking_time'] < 10 and len(recipe['ingredients']) >= 4:
-        recipe['difficulty'] = "Medium"
-    elif recipe['cooking_time'] >= 10 and len(recipe['ingredients']) < 4:
-        recipe['difficulty'] = "Intermediate"
-    elif recipe['cooking_time'] >= 10 and len(recipe['ingredients']) >= 4:
-        recipe['difficulty'] = "Hard"
+    if recipe["cooking_time"] < 10 and len(recipe["ingredients"]) < 4:
+        recipe["difficulty"] = "Easy"
+    elif recipe["cooking_time"] < 10 and len(recipe["ingredients"]) >= 4:
+        recipe["difficulty"] = "Medium"
+    elif recipe["cooking_time"] >= 10 and len(recipe["ingredients"]) < 4:
+        recipe["difficulty"] = "Intermediate"
+    elif recipe["cooking_time"] >= 10 and len(recipe["ingredients"]) >= 4:
+        recipe["difficulty"] = "Hard"
 
     print("==============================")
-    print("Added to your recipe: ", '\n', "Recipe:", recipe['name'], '\n', "Cooking Time (min): ", recipe['cooking_time'], '\n',
-          "Ingredients:", recipe['ingredients'], '\n', "Difficulty level:", recipe['difficulty'], '\n')
+    print(
+        "Added to your recipe: ",
+        "\n",
+        "Recipe:",
+        recipe["name"],
+        "\n",
+        "Cooking Time (min): ",
+        recipe["cooking_time"],
+        "\n",
+        "Ingredients:",
+        recipe["ingredients"],
+        "\n",
+        "Difficulty level:",
+        recipe["difficulty"],
+        "\n",
+    )
 
 print("All your ingredients: ")
 for ingredient in ingredients_list:
